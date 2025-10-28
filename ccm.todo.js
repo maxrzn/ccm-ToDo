057c74e265c0a8664ada45214243a89c4ee3d89d
maxrzn <Max.Ziegerer@gmail.com>
2025-10-28 22:14:28 +0100

    small local data structure added for testing

--- ccm.todo.js
+++ ccm.todo.js
@@ -3,14 +3,36 @@
 ccm.files['ccm.todo.js'] = {
     name: 'todo',
     ccm: '././libs/ccm/ccm.js',
-    config: {},
+    config: {
+        data: {
+            store: ['ccm.store', {
+                local: {
+                    notes: {                                //NOTE this lvl is this.data.key
+                        key: 'notes',
+                        title: 'Notizensammlung',
+                        items: [
+                            {
+                                topic: 'Einkaufsliste',
+                                content: 'Dies ist eine Einkaufsliste'
+                            },
+                            {
+                                topic: 'Schulnoten',
+                                content: 'Meine Schulnoten...'
+                            },
+                        ],
+                    },
+                },
+            }],
+            key: 'notes',
+        },
+    },
     html: ['ccm.load', '././resources/templates.html'],
 
-
     Instance: function () {
-        this.start = async() => {
-            const data = 'Hallo';
-            this.element.appendChild(this.ccm.helper.html(this.html.main, {test: data}));
+        this.start = async()=> {
+            const data = await this.data.store.get(this.data.key);   //NOTE sucht nach value von key also 'notes'
+            console.log(data, 'items', data.items, 'key', data.key);
+            this.element.appendChild(this.ccm.helper.html(this.html.main, {test: data.key}));  //TODO this.html.main undefined
         }
     }
 }
