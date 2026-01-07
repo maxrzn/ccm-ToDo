dec66f3eed221253b3a1db061e200ea4c7a4ed7f
maxrzn <Max.Ziegerer@gmail.com>
2026-01-07 14:16:03 +0100

    default kategorie erstellen, falls fuer den user noch keine existieren, delete all tasks und categories debug helper functions

--- ccm.todo.js
+++ ccm.todo.js
@@ -12,39 +12,66 @@ ccm.files['ccm.todo.js'] = {
             name: "mziege2s_tasks",
             url: "https://ccm2.inf.h-brs.de"
         }],
-        /*key: 'notes',*/
         html: ['ccm.load', '././resources/templates.html'],
+        css: ['ccm.load', '././resources/styles.css']
     },
 
 
     Instance: function () {
+        const userId = "testuser";
         /*this.ready = async() => {
             console.log('Dependencies loaded:', this.html);
         }*/
         this.start = async()=> {
-            await this.cat.set({
-                "key": "default",
-                "title": "Bachelorarbeit"
-            });
+            const userCats = await this.cat.get({ownerId: userId}); //check for existing categories
+            if(!userCats.length) {
+                await this.cat.set({    //create Default category
+                    title: "default",
+                    ownerId : userId,
+                    members : []
+                });
+            }
             const cats = await this.cat.get();
            /* await this.cat.del("default");*/
             cats.forEach((element) => {
                 console.log(element);
             });
-            await this.task.set({
+            /*await this.task.set({
                 key: "1",
                 content: "Komponente implementieren",
                 deadline: "2026-01-20",
                 points: 10,
                 status: "open",
                 categoryId: "default",
-                userId: "mziege2s"
-            })
+                userId: userId
+            })*/
+            const main = this.ccm.helper.html(this.html.main);
+
+            this.element.innerHTML = "";
+            this.element.appendChild((this.ccm.helper.html(this.html.header, {userId: userId})));
+            this.element.appendChild(main);
+
+            main.appendChild(this.ccm.helper.html(this.html.cat));
+            main.appendChild(this.ccm.helper.html(this.html.task));
 
             /*const data = await this.data.cat.get(this.data.key);   //NOTE get sucht nach value von key also 'notes' innerhalb von data
             console.log(data, 'items', data.items, 'key', data.key);
             this.element.innerHTML = '';
             this.element.appendChild(this.ccm.helper.html(this.html.main, {test: data.key}));*/
+        }
+        this.deleteAllCategories = async() => {
+            const cats = await this.cat.get();
+            cats.forEach((element) => {
+                this.cat.del(element.key);
+                console.log("Category: " + element.key + " wurde ausradiert!");
+            })
+        }
+        this.deleteAllTasks = async() => {
+            const tasks = await this.task.get();
+            tasks.forEach(element => {
+                this.task.del(element.key);
+                console.log("Task: " + element.key + " wurde ausradiert!");
+            })
         }
     }
 }
